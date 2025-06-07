c6a7e900a8cf914dbd0e13d3239b53180d4b8bb6
Matt Hill <mattnine@protonmail.com>
2025-06-07 14:33:46 -0600

    fix file path

--- startos/install/versions/v0.10.9.1.ts
+++ startos/install/versions/v0.10.9.1.ts
@@ -6,7 +6,9 @@ export const v0_10_9_1 = VersionInfo.of({
   releaseNotes: 'Revamped for StartOS 0.3.6',
   migrations: {
     up: async ({ effects }) => {
-      await rm('/data/start9', { recursive: true }).catch(console.error)
+      await rm('/media/startos/volumes/main/start9', { recursive: true }).catch(
+        console.error,
+      )
     },
     down: IMPOSSIBLE,
   },
