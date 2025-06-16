bf919dc36bb0b88ea24a6da552304a877a05e627
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-06-16 11:08:36 -0400

    beta30 backups fix

--- startos/backups.ts
+++ startos/backups.ts
@@ -2,7 +2,7 @@ import { sdk } from './sdk'
 
 export const { createBackup, restoreInit } = sdk.setupBackups(
   async ({ effects }) =>
-    sdk.Backups.volumes('main').setOptions({
+    sdk.Backups.ofVolumes('main').setOptions({
       exclude: ['/data/db'], // @TODO confirm path
     }),
 )
