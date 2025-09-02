a61cb33a2bb3c1140e998998b198539f0a66d194
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-02 17:28:51 -0600

    alpha.1

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,4 @@
-export { v0_10_10_0 as current } from './v0.10.10.0'
-export const other = []
+import { v0_10_10_0 } from "./v0.10.10.0";
+export { v0_10_10_0_alpha1 as current } from './v0.10.10.0_alpha1'
+
+export const other = [v0_10_10_0]
---
+++ startos/install/versions/v0.10.10.0_alpha1.ts
@@ -0,0 +1,10 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v0_10_10_0_alpha1 = VersionInfo.of({
+  version: '0.10.10:0-alpha.1',
+  releaseNotes: 'Updated for StartOS v0.4.0',
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
