c2ccd2ba2c65fcabb84d817c68730771ff34a984
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-02 16:11:28 -0600

    fix db backup exclusion

--- startos/backups.ts
+++ startos/backups.ts
@@ -3,6 +3,6 @@ import { sdk } from './sdk'
 export const { createBackup, restoreInit } = sdk.setupBackups(
   async ({ effects }) =>
     sdk.Backups.ofVolumes('main').setOptions({
-      exclude: ['/data/db'], // @TODO confirm path
+      exclude: ['/db'],
     }),
 )
