3e76de743eeb1bbaee25be84f43b97c2a044d817
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-18 13:28:16 -0600

    add missing arch from hardwareRequirements

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -34,7 +34,9 @@ export const manifest = setupManifest({
       arch: architectures,
     } as SDKImageInputSpec,
   },
-  hardwareRequirements: {},
+  hardwareRequirements: {
+    arch: architectures,
+  },
   alerts: {
     install: null,
     update: null,
