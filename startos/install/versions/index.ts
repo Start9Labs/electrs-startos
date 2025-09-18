319dc5f9aa502b35e79aeeb541d6e31cf1969955
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-18 13:13:46 -0600

    bump package pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
 import { v0_10_10_0 } from "./v0.10.10.0";
-export { v0_10_10_0_alpha1 as current } from './v0.10.10.0_alpha1'
+export { v0_10_10_0_alpha2 as current } from './v0.10.10.0_alpha2'
 
 export const other = [v0_10_10_0]
