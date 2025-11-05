a83df51db7f4a3549a438c84a19e51e1f2db8b32
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-05 15:10:51 -0700

    bump startos package major version and pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
 import { v0_10_10_0 } from "./v0.10.10.0";
-export { v0_10_10_0_alpha2 as current } from './v0.10.10.0_alpha2'
+export { v0_10_10_1_beta0 as current } from './v0.10.10.1_beta0'
 
 export const other = [v0_10_10_0]
--- startos/install/versions/v0.10.10.1_beta0.ts
+++ startos/install/versions/v0.10.10.1_beta0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v0_10_10_0_alpha2 = VersionInfo.of({
-  version: '0.10.10:0-alpha.2',
+export const v0_10_10_1_beta0 = VersionInfo.of({
+  version: '0.10.10:1-beta.0',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {},
