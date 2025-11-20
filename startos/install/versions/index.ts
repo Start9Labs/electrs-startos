858a7bd0428c912cce8868a863f94e03fd191e2a
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-19 22:34:14 -0700

    update electrs to 0.11

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
 import { v0_10_10_0 } from "./v0.10.10.0";
-export { v0_10_10_1_beta0 as current } from './v0.10.10.1_beta0'
+export { v0_11_0_1_beta0 as current } from './v0.11.0.1_beta0'
 
 export const other = [v0_10_10_0]
--- startos/install/versions/v0.11.0.1_beta0.ts
+++ startos/install/versions/v0.11.0.1_beta0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v0_10_10_1_beta0 = VersionInfo.of({
-  version: '0.10.10:1-beta.0',
+export const v0_11_0_1_beta0 = VersionInfo.of({
+  version: '0.11.0:1-beta.0',
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {},
