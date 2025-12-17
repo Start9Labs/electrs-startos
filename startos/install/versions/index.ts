297b956550d143d21e818622b283a9f5d6c5a624
Shadowy Super Coder <musashidisciple@proton.me>
2025-12-16 19:43:23 -0700

    consolidate versions and bump pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,3 @@
-import { v0_10_10_0 } from "./v0.10.10.0";
-export { v0_11_0_1_beta0 as current } from './v0.11.0.1_beta0'
+export { v0_11_0_1 as current } from './v0.11.0.1'
 
-export const other = [v0_10_10_0]
+export const other = []
