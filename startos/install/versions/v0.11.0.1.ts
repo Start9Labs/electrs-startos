8ad89b1cbb8e6356a03d816225134c6b5e389752
Matt Hill <mattnine@protonmail.com>
2025-12-21 18:23:46 -0700

    better migration

--- startos/install/versions/v0.11.0.1.ts
+++ startos/install/versions/v0.11.0.1.ts
@@ -1,6 +1,5 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
 import { readFile, rm } from 'fs/promises'
-import { load } from 'js-yaml'
 import { LogFilters } from '../../utils'
 import { tomlFile } from '../../fileModels/electrs.toml'
 import { configDefaults } from '../../utils'
@@ -18,27 +17,28 @@ export const v0_11_0_1 = VersionInfo.of({
   releaseNotes: 'Updated for StartOS v0.4.0',
   migrations: {
     up: async ({ effects }) => {
-      const oldConfigFile = await readFile(
+      // get old config.yaml
+      const configYaml:
+        | {
+            'log-filters': LogFilters
+            'index-batch-size': number
+            'index-lookup-limit': number
+          }
+        | undefined = await readFile(
         '/media/startos/volumes/main/start9/config.yaml',
         'utf-8',
-      ).catch(console.log)
+      ).then(YAML.parse, () => undefined)
 
-      if (oldConfigFile) {
-        const oldConfig = load(oldConfigFile) as {
-          'log-filters': LogFilters
-          'index-batch-size': number
-          'index-lookup-limit': number
-        }
-
+      if (configYaml) {
         await tomlFile.write(effects, {
           cookie_file,
           daemon_rpc_addr,
           daemon_p2p_addr,
           electrum_rpc_addr,
           network,
-          log_filters: oldConfig['log-filters'],
-          index_batch_size: oldConfig['index-batch-size'],
-          index_lookup_limit: oldConfig['index-lookup-limit'],
+          log_filters: configYaml['log-filters'],
+          index_batch_size: configYaml['index-batch-size'],
+          index_lookup_limit: configYaml['index-lookup-limit'],
         })
 
         // remove old start9 dir
